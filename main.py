a874262f744cd094b0c3541a553a51577ed1fc81
Joshua Amarfio <53336997+Amarfio@users.noreply.github.com>
2024-08-23 04:07:09 +0000

    first code push

---
+++ main.py
@@ -0,0 +1,14 @@
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+#
+#
+# file.close()
+
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# with open("C:/Users/USGL/Desktop/my_file.txt", mode="w") as file:
+with open("../../Desktop/my_file.txt", mode="w") as file:
+    file.write("New text. work again")
